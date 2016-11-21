791067ca4ed1c2b3fdec2a13e955fb744751c527
Katy Farmer <dreadpiratekaty@gmail.com>
2016-11-20 18:49:45 -0700

    Added custom cipher rotation

--- encryptor.rb
+++ encryptor.rb
@@ -1,31 +1,34 @@
 class Encryptor
+  def initialize(rotation)
+    @rotation = rotation
+  end
+
   def cipher(rotation)
     characters = (" ".."z").to_a
     rotated_characters = characters.rotate(rotation)
     Hash[characters.zip(rotated_characters)]
-   end
+  end
 
-   def encrypt_letter(letter)
-     rotation = 13
-     cipher_for_rotation = cipher(rotation)
-     cipher_for_rotation[letter]
-   end
+  def encrypt_letter(letter)
+    cipher_for_rotation = cipher(@rotation)
+    cipher_for_rotation[letter]
+  end
 
-   def encrypt(string)
-     letters = string.split("")
-     new_string = ""
-     letters.each {|letter| new_string += encrypt_letter(letter)}
-     new_string
-   end
+  def encrypt(string)
+    letters = string.split("")
+    new_string = ""
+    letters.each {|letter| new_string += encrypt_letter(letter)}
+    new_string
+  end
 
-   def decrypt_letter(letter)
-     cipher.invert[letter]
-   end
+  def decrypt_letter(letter)
+    cipher.invert[letter]
+  end
 
-   def decrypt(string)
-     letters = string.split("")
-     new_string = ""
-     letters.each {|letter| new_string += encrypt_letter(letter)}
-     new_string
-   end
+  def decrypt(string)
+    letters = string.split("")
+    new_string = ""
+    letters.each {|letter| new_string += encrypt_letter(letter)}
+    new_string
+  end
 end
